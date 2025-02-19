bf45b3724d6f617934f585c17aafecf9332c5868
imprathamraj <rjpratham05@gmail.com>
2025-02-19 11:47:29 +0530

    [Pratham Raj] Add. Create a JSON object for a Student with fields: name, age, and subjects (array).

---
+++ Day_02/src/main/java/com/json/handsonpracticeproblems/createajsonobjectforastudentwithfields/CreatingAJSONObjectForAStudentWithFields.java
@@ -0,0 +1,22 @@
+package com.json.handsonpracticeproblems.createajsonobjectforastudentwithfields;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class CreatingAJSONObjectForAStudentWithFields {
+    public static void main(String[]args){
+        // Creating json object
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name", "Pratham");
+        jsonObject.put("age", 22);
+        // Creating json array to store subjects
+        JSONArray subjects = new JSONArray();
+        subjects.put("DBMS");
+        subjects.put("OOPS");
+        subjects.put("CLOUD");
+        subjects.put("AI");
+        jsonObject.put("subject", subjects);
+        // Output
+        System.out.println(jsonObject.toString(4));
+    }
+}
