46fbc10fa299a035a9dc5c18bbb3f1894b07b058
imprathamraj <rjpratham05@gmail.com>
2025-02-19 12:41:57 +0530

    [Pratham Raj] Add. Convert a Java object (Car) into JSON format.

---
+++ Day_02/src/main/java/com/json/handsonpracticeproblems/convertajavaobjectcarintojsonformat/ConvertingAJavaObjectCarIntoJSONFormat.java
@@ -0,0 +1,27 @@
+package com.json.handsonpracticeproblems.convertajavaobjectcarintojsonformat;
+
+import org.json.JSONObject;
+
+class Car {
+    //Fields
+    public String brand;
+    public int year;
+
+    //Constructor
+    public Car(String brand, int year) {
+        this.brand = brand;
+        this.year = year;
+    }
+}
+public class ConvertingAJavaObjectCarIntoJSONFormat {
+    public static void main(String[]args){
+        //Creating Car class car object
+        Car car = new Car("Tesla Model Z", 2024);
+        JSONObject jsonObject = new JSONObject();
+
+        //Creating json object
+        jsonObject.put("brand",car.brand);
+        jsonObject.put("year",car.year);
+        System.out.println(jsonObject.toString(5));
+    }
+}
