89210530052a636b75549333d5e4295120a31f79
imprathamraj <rjpratham05@gmail.com>
2025-02-19 16:13:46 +0530

    [Pratham Raj] Add. Merge two JSON objects into one.

---
+++ Day_02/src/main/java/com/json/practiceproblems/mergetwojsonobjectsintoone/MergingTwoJson.java
@@ -0,0 +1,34 @@
+/*Merge two JSON objects into one.*/
+package com.json.practiceproblems.mergetwojsonobjectsintoone;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class MergingTwoJson {
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        //creating the objrct
+        JSONObject obj1= new JSONObject();
+        //putting the key value in the object
+        obj1.put("name","Pratham Raj");
+        obj1.put("age",22);
+
+        // creating the array of subjects
+        JSONArray objArr = new JSONArray();
+        objArr.put("COA").put("ADDA").put("CLOUD");
+        obj1.put("subjects",objArr);
+
+        //creating the objrct
+        JSONObject obj2= new JSONObject();
+        //putting the key value in the object
+        obj2.put("Email","pratham@gmail.com");
+        obj2.put("DOB","09-08-2004");
+
+        obj2.keySet().forEach(key->obj1.put(key,obj2.get(key)));
+        System.out.println(obj1.toString(5));
+
+
+    }
+}
